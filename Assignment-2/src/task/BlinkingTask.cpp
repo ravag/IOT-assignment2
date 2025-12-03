c2eb5ea0c0500b040bc43aaa0b2cc73cad8c1664
Marco Bulgarelli - marco.bulgarelli6@studio.unibo.it <marco.bulgarelli6@studio.unibo.it>
2025-12-03 14:47:41 +0100

    Added Blinking Task Implementation

---
+++ Assignment-2/src/task/BlinkingTask.cpp
@@ -0,0 +1,71 @@
+#include "Task\BlinkingTask.h"
+#include "config.h"
+#include <Arduino.h>
+#include "kernel\Logger.h"
+
+BlinkingTask::BlinkingTask(Led* pLed, Context* pContext):
+    pContext(pContext), pLed(pLed){
+        setState(IDLE);
+    }
+
+void BlinkingTask::tick(){
+    switch (state){
+        case IDLE: {
+            if(this->checkAndSetJustEntered()){
+                pLed->switchOff();
+                Logger.log(F("[BT] IDLE"));
+            }
+
+            if(pContext->isInAlarm){
+                setState(OFF);
+            }
+
+            break;
+        }
+
+        case OFF: {
+            if(this->checkAndSetJustEntered()){
+                pLed->switchOff();
+                Logger.log(F("[BT] IDLE"));
+            }
+
+            if(pContext->isInAlarm){
+                setState(IDLE);
+            } else {
+                setState(ON);
+            }
+
+            break;
+        }
+
+        case ON: {
+            if(this->checkAndSetJustEntered()){
+                pLed->switchOn();
+                Logger.log(F("[BT] IDLE"));
+            }
+
+            if(pContext->isInAlarm){
+                setState(IDLE);
+            } else {
+                setState(OFF);
+            }
+
+            break;
+        }
+    }
+}
+
+void BlinkingTask::setState(int s){
+    state = s;
+    justEntered = true; 
+}
+
+bool BlinkingTask::checkAndSetJustEntered(){
+    bool bak = justEntered;
+
+    if(justEntered){
+        justEntered = false;
+    }
+
+    return bak;
+}
