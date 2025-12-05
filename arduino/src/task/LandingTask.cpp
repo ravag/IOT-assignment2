5bb1d5285b0b6bac31642ca09ee17740ec49f917
Marco Bulgarelli - marco.bulgarelli6@studio.unibo.it <marco.bulgarelli6@studio.unibo.it>
2025-12-05 16:24:11 +0100

    updated LandingTask

--- arduino/src/task/LandingTask.cpp
+++ arduino/src/task/LandingTask.cpp
@@ -9,120 +9,122 @@ pMotor(pMotor), pPIR(pPIR), pSonar(pSonar), pLCD(pLCD), pContext(pContext) {
 }
 
 void LandingTask::tick(){
-    switch(state){
-        case IDLE: {
-            if(this->checkAndSetJustEntered()){
-                pLCD->clear();
-                pLCD->print("DRONE IN");
-                Logger.log(F("[LT] IDLE"));
-            }
-
-            if(pContext->isInAlarm()) {
-                setState(DOOR_CLOSING);
-            }
-            msg = MsgService.receiveMsg(); 
-            if(!pContext->isInPreAlarm() && msg->getContent() == "rl" && this->isDroneNear()) {
-                setState(DOOR_OPENING);
-            }
-
-            break;
-        }
-
-        case DOOR_OPENING: {
-            if(this->checkAndSetJustEntered()){     
-                pContext->setBlinkingOn();
-                pLCD->clear();
-                pLCD->print("LANDING");
-                pMotor->on();
-                Logger.log(F("[LT] DOOR OPENING"));
-            }
-            this->openDoor();
-
-            if(pContext->isInAlarm()) {
-                setState(DOOR_CLOSING);
-            }
-
-            if(this->isDoorOpen()) {
-                setState(DOOR_OPEN);
-            } else {
-                setState(DOOR_OPENING);
-            }
-
-            break;
-        }
-
-        case DOOR_OPEN: {
-            if(this->checkAndSetJustEntered()){
-                Logger.log(F("[LT] DOOR OPEN"));
-            }
-
-            if(pContext->isInAlarm()) {
-                setState(DOOR_CLOSING);
-            }
-
-            if(pPIR->isDetected()) {
-                setState(DRONE_LANDED);
-            } else {
-                setState(DOOR_OPEN);
-            }
-
-            break;
-        }
-
-        case DRONE_LANDED: {
-            if(this->checkAndSetJustEntered()){
-                Logger.log(F("[LT] DRONE LANDED"));
-            }
-
-            if(pContext->isInAlarm()) {
-                setState(DOOR_CLOSING);
-            }
-
-            if(!this->isDroneNear()) {
-                setState(DOOR_OPEN);
-            } else {
-                if(lastTime == 0){
-                    lastTime = millis();
+    if (pContext->isDroneIn()) {
+        switch(state){
+            case IDLE: {
+                if(this->checkAndSetJustEntered()){
+                    pLCD->clear();
+                    pLCD->print("DRONE IN");
+                    Logger.log(F("[LT] IDLE"));
                 }
-                if(this->isDroneNear()) {
-                    if(millis() - lastTime >= DISTANCE_TIME) {
-                        setState(DOOR_CLOSING);
+
+                if(pContext->isInAlarm()) {
+                    setState(DOOR_CLOSING);
+                }
+                msg = MsgService.receiveMsg(); 
+                if(!pContext->isInPreAlarm() && msg->getContent() == "rl" && this->isDroneNear()) {
+                    setState(DOOR_OPENING);
+                }
+
+                break;
+            }
+
+            case DOOR_OPENING: {
+                if(this->checkAndSetJustEntered()){     
+                    pContext->setBlinkingOn();
+                    pLCD->clear();
+                    pLCD->print("LANDING");
+                    pMotor->on();
+                    Logger.log(F("[LT] DOOR OPENING"));
+                }
+                this->openDoor();
+
+                if(pContext->isInAlarm()) {
+                    setState(DOOR_CLOSING);
+                }
+
+                if(this->isDoorOpen()) {
+                    setState(DOOR_OPEN);
+                } else {
+                    setState(DOOR_OPENING);
+                }
+
+                break;
+            }
+
+            case DOOR_OPEN: {
+                if(this->checkAndSetJustEntered()){
+                    Logger.log(F("[LT] DOOR OPEN"));
+                }
+
+                if(pContext->isInAlarm()) {
+                    setState(DOOR_CLOSING);
+                }
+
+                if(pPIR->isDetected()) {
+                    setState(DRONE_LANDED);
+                } else {
+                    setState(DOOR_OPEN);
+                }
+
+                break;
+            }
+
+            case DRONE_LANDED: {
+                if(this->checkAndSetJustEntered()){
+                    Logger.log(F("[LT] DRONE LANDED"));
+                }
+
+                if(pContext->isInAlarm()) {
+                    setState(DOOR_CLOSING);
+                }
+
+                if(!this->isDroneNear()) {
+                    setState(DOOR_OPEN);
+                } else {
+                    if(lastTime == 0){
+                        lastTime = millis();
+                    }
+                    if(this->isDroneNear()) {
+                        if(millis() - lastTime >= DISTANCE_TIME) {
+                            setState(DOOR_CLOSING);
+                        }
                     }
                 }
+
+                break;
             }
 
-            break;
-        }
+            case DOOR_CLOSING: {
+                if(this->checkAndSetJustEntered()){   
+                    pContext->setBlinkingOn();    
+                    pMotor->on();     
+                    Logger.log(F("[LT] DOOR CLOSING"));
+                }
+                this->closeDoor();
 
-        case DOOR_CLOSING: {
-            if(this->checkAndSetJustEntered()){   
-                pContext->setBlinkingOn();    
-                pMotor->on();     
-                Logger.log(F("[LT] DOOR CLOSING"));
-            }
-            this->closeDoor();
+                if(!this->isDoorOpen() && pContext->isInAlarm()) {
+                    setState(ALARM);
+                } else if(!this->isDoorOpen() && !pContext->isInAlarm()) {
+                    setState(IDLE);
+                } else {
+                    setState(DOOR_CLOSING);
+                }
 
-            if(!this->isDoorOpen() && pContext->isInAlarm()) {
-                setState(ALARM);
-            } else if(!this->isDoorOpen() && !pContext->isInAlarm()) {
-                setState(IDLE);
-            } else {
-                setState(DOOR_CLOSING);
-            }
-
-            break;
-        }
-
-        case ALARM: {
-            if(this->checkAndSetJustEntered()){
-                Logger.log(F("[LT] ALARM"));
+                break;
             }
 
-            if(!pContext->isInAlarm()){
-                setState(IDLE);
-            }
+            case ALARM: {
+                if(this->checkAndSetJustEntered()){
+                    Logger.log(F("[LT] ALARM"));
+                }
 
-            break;
+                if(!pContext->isInAlarm()){
+                    setState(IDLE);
+                }
+
+                break;
+            }
         }
     }
 }
