3508200a2ab74fbcd803e67767dabd12e1e2d16a
Alessandro Ravaioli - alessandro.ravaioli8@studio.unibo.it <alessandro.ravaioli8@studio.unibo.it>
2025-12-05 16:30:41 +0100

    Change to takeofftask

--- arduino/src/task/TakeOffTask.cpp
+++ arduino/src/task/TakeOffTask.cpp
@@ -20,126 +20,126 @@ void TakeOffTask::tick() {
     if (this->context->isDroneIn())
     {
     
-    if (this->context->isInAlarm() && !alarm)
-    {
-        this->setState(CLOSING);
-        this->alarm = true;
-    }
-    
-    switch (this->state)
-    {
-        case IDLE:
-            if (justEntered)
-            {
-                justEntered = false;
-                Logger.log("[TakeOffTask]: Entered Idle State");
-            }
-            
-            msg = MsgService.receiveMsg();
-            if (msg != NULL)
-            {
-                if (msg->getContent() == "rtk" && !this->context->isInPreAlarm())
+        if (this->context->isInAlarm() && !alarm)
+        {
+            this->setState(CLOSING);
+            this->alarm = true;
+        }
+        
+        switch (this->state)
+        {
+            case IDLE:
+                if (justEntered)
                 {
-                    this->setState(OPENING);
-                    this->context->setBlinkingOn();
-                    lcd->clear();
-                    lcd->setCursor(2,1);
-                    lcd->print("TAKE OFF");
+                    justEntered = false;
+                    Logger.log("[TakeOffTask]: Entered Idle State");
                 }
                 
-            }
-            
-            break;
-
-        case OPENING:
-            if (justEntered)
-            {
-                this->motor->on();
-                justEntered = false;
-                Logger.log("lo[TakeOffTask]: Entered Opening State");
-            }
-            
-            dt = millis() - timeInState;
-            motor->setPosition(((float)(dt/TIME_TO_OPEN))*90);
-
-            if (this->motor->getPosition() >= 90)
-            {
-                this->setState(OPEN);
-            }
-            
-            break;
-
-        case OPEN:
-            if (justEntered)
-            {
-                this->motor->off();
-                justEntered = false;
-                Logger.log("lo[TakeOffTask]: Entered Open State");
-                alreadyOver = false;
-            }
-
-            sensor->setTemperature(temp->getTemperature());
-            dist = sensor->getDistance();
-            /*se sei sopra una distaza d per t secondi comincia a chiudere*/
-            Serial.print("lo");
-            Serial.println(dist);
-            if (dist > CLOSING_DISTANCE || dist == NO_OBJ_DETECTED) {
-                if (alreadyOver) {
-                    timePass = millis() - lastTime;
-                    if (timePass > DISTANCE_TIME) {
-                        Logger.log("lo[TakeOffTask]: time passed -> " + timePass);
-                        this->setState(CLOSING);
+                msg = MsgService.receiveMsg();
+                if (msg != NULL)
+                {
+                    if (msg->getContent() == "rtk" && !this->context->isInPreAlarm())
+                    {
+                        this->setState(OPENING);
+                        this->context->setBlinkingOn();
                         lcd->clear();
                         lcd->setCursor(2,1);
-                        lcd->print("DRONE OUT");
-                        this->context->setDroneOut();
+                        lcd->print("TAKE OFF");
+                    }
+                    
+                }
+                
+                break;
+
+            case OPENING:
+                if (justEntered)
+                {
+                    this->motor->on();
+                    justEntered = false;
+                    Logger.log("lo[TakeOffTask]: Entered Opening State");
+                }
+                
+                dt = millis() - timeInState;
+                motor->setPosition(((float)(dt/TIME_TO_OPEN))*90);
+
+                if (this->motor->getPosition() >= 90)
+                {
+                    this->setState(OPEN);
+                }
+                
+                break;
+
+            case OPEN:
+                if (justEntered)
+                {
+                    this->motor->off();
+                    justEntered = false;
+                    Logger.log("lo[TakeOffTask]: Entered Open State");
+                    alreadyOver = false;
+                }
+
+                sensor->setTemperature(temp->getTemperature());
+                dist = sensor->getDistance();
+                /*se sei sopra una distaza d per t secondi comincia a chiudere*/
+                Serial.print("lo");
+                Serial.println(dist);
+                if (dist > CLOSING_DISTANCE || dist == NO_OBJ_DETECTED) {
+                    if (alreadyOver) {
+                        timePass = millis() - lastTime;
+                        if (timePass > DISTANCE_TIME) {
+                            Logger.log("lo[TakeOffTask]: time passed -> " + timePass);
+                            this->setState(CLOSING);
+                            lcd->clear();
+                            lcd->setCursor(2,1);
+                            lcd->print("DRONE OUT");
+                            this->context->setDroneOut();
+                        }
+                        
+                    } else {
+                        timePass = 0;
+                        lastTime = millis();
+                        alreadyOver = true;
                     }
                     
                 } else {
-                    timePass = 0;
-                    lastTime = millis();
-                    alreadyOver = true;
+                    alreadyOver = false;
                 }
                 
-            } else {
-                alreadyOver = false;
-            }
-            
-            break;
+                break;
 
-        case CLOSING:
-            if(justEntered)
-            {
-                this->motor->on();
-                justEntered = false;
-                Logger.log("lo[TakeOffTask]: Entered Closing State");
-            }
+            case CLOSING:
+                if(justEntered)
+                {
+                    this->motor->on();
+                    justEntered = false;
+                    Logger.log("lo[TakeOffTask]: Entered Closing State");
+                }
 
-            dt = millis() - timeInState;
-            motor->setPosition( 90 - ((float)(dt/TIME_TO_OPEN) * 90));
+                dt = millis() - timeInState;
+                motor->setPosition( 90 - ((float)(dt/TIME_TO_OPEN) * 90));
 
-            if (motor->getPosition() == 0)
-            {
-                this->motor->off();
-                context->setBlinkingOff();
-                this->setState(this->context->isInAlarm() ? ALARM : IDLE);
-            }
+                if (motor->getPosition() == 0)
+                {
+                    this->motor->off();
+                    context->setBlinkingOff();
+                    this->setState(this->context->isInAlarm() ? ALARM : IDLE);
+                }
+                
+                break;
+            case ALARM:
+                if (justEntered)
+                {
+                    justEntered = false;
+                    Logger.log("lo[TakeOffTask]: Entered Alarm State");
+                }
             
-            break;
-        case ALARM:
-            if (justEntered)
-            {
-                justEntered = false;
-                Logger.log("lo[TakeOffTask]: Entered Alarm State");
-            }
-        
-            if (!this->context->isInAlarm())
-            {
-                this->setState(IDLE);
-                this->alarm = false;
-            }
-            
-            break;
-    }
+                if (!this->context->isInAlarm())
+                {
+                    this->setState(IDLE);
+                    this->alarm = false;
+                }
+                
+                break;
+        }
     }
 }
