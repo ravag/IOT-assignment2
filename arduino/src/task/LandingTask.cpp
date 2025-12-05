fc9fa2d2c786ebd74c15804b62cd5e93bef8f479
Marco Bulgarelli - marco.bulgarelli6@studio.unibo.it <marco.bulgarelli6@studio.unibo.it>
2025-12-05 15:29:49 +0100

    added log message to Landing Task

--- arduino/src/task/LandingTask.cpp
+++ arduino/src/task/LandingTask.cpp
@@ -167,5 +167,8 @@ bool LandingTask::isDoorOpen(){
 }
 
 bool LandingTask::isDroneNear(){
+    if(pPIR->isDetected()){
+        Logger.log(F("[LT] DRONE DETECTED"));
+    }
     return pSonar->getDistance() < CLOSING_DISTANCE;
 }
